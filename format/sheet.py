8c07fbee39226cda236b371fe6488892e3e64c68
Bruno Santiago <brunomsantiago@gmail.com>
2023-02-09 00:37:09 -0300

    find groups limits

--- format/sheet.py
+++ format/sheet.py
@@ -4,6 +4,12 @@ from format.group import Group
 from format.column import Column
 
 
+def indexes(element, my_list):
+    first = my_list.index(element) + 1
+    last = len(my_list) - my_list[::-1].index(element)
+    return first, last
+
+
 class Sheet:
     def __init__(self,
                  sheet_name: str,
@@ -12,6 +18,14 @@ class Sheet:
         self.sheet_name = sheet_name
         self.groups = groups
         self.columns = columns
+
+    def groups_limits(self):
+        cols_groups = [col.group.group_name for col in self.columns.values()]
+        groups_names = list(dict.fromkeys(cols_groups))
+        return {
+            group_name: indexes(group_name, cols_groups)
+            for group_name
+            in groups_names}
 
     def __repr__(self):
         s = []
