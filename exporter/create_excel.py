1963e0b36a36fd85b1b584150e7a344da21b4a02
Bruno Santiago <brunomsantiago@gmail.com>
2023-02-09 20:56:04 -0300

    bug fix: forget the () calling a dict.values()

--- exporter/create_excel.py
+++ exporter/create_excel.py
@@ -8,9 +8,10 @@ from exporter.write_sheet import write_sheet
 
 def export_excel(data: List[Dict], config: ExcelConfiguration, update_time):
     wb = Workbook()
-    for n, (ws_data, ws_config) in enumerate(zip(data, config.sheets.values)):
+    for n, (ws_data, ws_config) in enumerate(zip(data, config.sheets.values())):
         ws = wb.active if n == 0 else wb.create_sheet()
         write_sheet(ws, ws_data, ws_config, update_time)
     virtual_file = BytesIO()
     wb.save(virtual_file)
     virtual_file.seek(0)
+    return virtual_file
