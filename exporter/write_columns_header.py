0c2f1da2de1b3d439ecd39a22464bc1799f003fa
Bruno Santiago <brunomsantiago@gmail.com>
2023-02-10 10:27:22 -0300

    comments and columns widths

--- exporter/write_columns_header.py
+++ exporter/write_columns_header.py
@@ -1,6 +1,7 @@
 from typing import Dict
 
 from openpyxl.styles import Alignment, Font, PatternFill
+from openpyxl.utils import get_column_letter
 from openpyxl.worksheet.worksheet import Worksheet
 
 from configuration.column import Column as ColumnConfiguration
@@ -16,12 +17,19 @@ def write_columns_header(ws: Worksheet,
     font = Font(bold=True, color='FFFFFF')
     for col_number, col_config in enumerate(columns_config.values(),
                                             start=start_col):
+        # Write column name
+        cell = ws.cell(row=row_number, column=col_number)
+        cell.value = col_config.column_name
+        # Format column background color in the header
         color = col_config.group.background_color.lstrip('#')
         fill = PatternFill(start_color=color,
                            end_color=color,
                            fill_type='solid')
-        cell = ws.cell(row=row_number, column=col_number)
-        cell.value = col_config.column_name
         cell.fill = fill
-        cell.alignment = alignment
+        # Format column font (bold, color) in the header
         cell.font = font
+        # Format column alignment in the header
+        cell.alignment = alignment
+        # Adjust column width
+        col_letter = get_column_letter(col_number)
+        ws.column_dimensions[col_letter].width = col_config.column_width
